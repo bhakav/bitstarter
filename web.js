414d149afd04ebc3b04a7462b08924d6e6f1fcb9
Bharat Sharma <bhakav@gmail.com>
2013-07-11 01:14:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 
-var buf=new Buffer(fs.readFileSync("index.html");
+var buf=new Buffer(fs.readFileSync("index.html"));
 
 
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('urf8');
+  response.send(buf.toString('urf8'));
 });
 
 var port = process.env.PORT || 5000;

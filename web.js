623de201a7aaeeb26b05f3683dabbca9ed0c37a2
Bharat Sharma <bhakav@gmail.com>
2013-07-11 01:05:27 +0000

    Added Index

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var buf=new Buffer(fs.readFileSync("index.html");
+
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString('urf8');
 });
 
 var port = process.env.PORT || 5000;
